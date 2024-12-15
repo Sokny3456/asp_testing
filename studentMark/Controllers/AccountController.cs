e0e709888e15e32edba6f412ee2304f8071cc8d6
Sokny3456 <rithsokny3456@gmail.com>
2024-12-15 09:31:51 +0700

    test asp

---
+++ studentMark/Controllers/AccountController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace studentMark.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(string uname, string pword)
+        {
+           
+            if (uname == "admin" && pword == "123")
+            {
+                Session["uname"] = "admin";
+                return RedirectToAction("Personal", "Student");
+            }
+
+            // Display error message on failure
+            ViewBag.ErrorMessage = "Invalid login attempt.";
+            return View();
+        }
+
+
+        // GET: Account/Register
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        // POST: Account/Register
+        [HttpPost]
+        public ActionResult Register(string uname, string pword)
+        {
+           
+            return RedirectToAction("Login");
+        }
+
+        // GET: Account/Logout
+        public ActionResult Logout()
+        {
+            
+            return RedirectToAction("Login");
+        }
+    
+
+
+        
+    }
+}
---
+++ studentMark/Controllers/StudentController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace studentMark.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ViewResult Personal() {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Personal(string sname, string contact, string address)
+        {
+            if (sname == "" || contact == "" || address == "")
+            {
+                ViewBag.msg = "Pleas enter all information";
+                return View();
+            }
+            else {
+                TempData["name"] = sname;
+                TempData["address"] = address;
+                TempData["contact"]= contact;
+                return RedirectToAction("marks");
+            }
+            
+        }
+        public ViewResult Marks() { 
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Marks(int theory = 0, int pracs = 0, int oral = 0) {
+            if (theory < 0 || theory > 100 || pracs < 0 || pracs > 100 || oral < 0 || oral > 100)
+            {
+                ViewData["msg"] = "Invalid marks, please enter between 0-100";
+                return View();
+            }
+            else {
+                TempData["theory"] = theory;
+                TempData["pracs"]= pracs;
+                TempData["oral"]= oral;
+                return RedirectToAction("DisplayResult");
+            }
+
+        }
+        [Route("marksheet")]
+        public ViewResult DisplayResult() {
+
+            return View();
+        }
+    }
+}
